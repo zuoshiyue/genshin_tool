85abe199f0698de2b156b0e2c89c14314e8b3032
lupengfei3 <lupengfei3@guazi.com>
2022-08-03 16:24:06 +0800

    Initial commit

---
+++ src/main/java/com/zuoshiyue/genshin/genshin_tool/GenshinToolApplication.java
@@ -0,0 +1,13 @@
+package com.zuoshiyue.genshin.genshin_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenshinToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenshinToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuoshiyue/genshin/genshin_tool/GenshinToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuoshiyue.genshin.genshin_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenshinToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
