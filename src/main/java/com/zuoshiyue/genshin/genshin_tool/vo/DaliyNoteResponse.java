e1b7a6e524085e3e98776f8d7f76d28fde28af9e
lupengfei3 <lupengfei3@guazi.com>
2022-08-08 14:28:52 +0800

    添加标签参数注释

--- src/main/java/com/zuoshiyue/genshin/genshin_tool/vo/DaliyNoteResponse.java
+++ src/main/java/com/zuoshiyue/genshin/genshin_tool/vo/DaliyNoteResponse.java
@@ -15,10 +15,19 @@ import java.util.List;
 @Data
 public class DaliyNoteResponse {
 
+    /**
+     * 当前树脂
+     */
     @JsonProperty("current_resin")
     private Integer currentResin;
+    /**
+     * 树脂上限
+     */
     @JsonProperty("max_resin")
     private Integer maxResin;
+    /**
+     *  树脂预计恢复时间
+     */
     @JsonProperty("resin_recovery_time")
     private String resinRecoveryTime;
     /**
@@ -31,22 +40,49 @@ public class DaliyNoteResponse {
      */
     @JsonProperty("total_task_num")
     private Integer totalTaskNum;
+    /**
+     * 每日委托额外奖励领取情况
+     */
     @JsonProperty("is_extra_task_reward_received")
     private Boolean isExtraTaskRewardReceived;
+    /**
+     * 强敌每周减半次数剩余
+     */
     @JsonProperty("remain_resin_discount_num")
     private Integer remainResinDiscountNum;
+    /**
+     * 强敌每周减半次数上限
+     */
     @JsonProperty("resin_discount_num_limit")
     private Integer resinDiscountNumLimit;
+    /**
+     * 当前探索派遣人数
+     */
     @JsonProperty("current_expedition_num")
     private Integer currentExpeditionNum;
+    /**
+     * 探索派遣限制
+     */
     @JsonProperty("max_expedition_num")
     private Integer maxExpeditionNum;
+    /**
+     * 派遣人员详情
+     */
     @JsonProperty("expeditions")
     private List<ExpeditionsDTO> expeditions;
+    /**
+     * 当前洞天宝钱
+     */
     @JsonProperty("current_home_coin")
     private Integer currentHomeCoin;
+    /**
+     * 洞天宝钱上限
+     */
     @JsonProperty("max_home_coin")
     private Integer maxHomeCoin;
+    /**
+     * 洞天宝钱预计恢复时间
+     */
     @JsonProperty("home_coin_recovery_time")
     private String homeCoinRecoveryTime;
     @JsonProperty("calendar_url")
@@ -87,10 +123,19 @@ public class DaliyNoteResponse {
     @NoArgsConstructor
     @Data
     public static class ExpeditionsDTO {
+        /**
+         * 图标
+         */
         @JsonProperty("avatar_side_icon")
         private String avatarSideIcon;
+        /**
+         * 派遣状态
+         */
         @JsonProperty("status")
         private String status;
+        /**
+         * 派遣剩余时间
+         */
         @JsonProperty("remained_time")
         private String remainedTime;
     }
