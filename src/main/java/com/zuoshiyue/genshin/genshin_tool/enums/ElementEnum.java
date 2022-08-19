933c3c1e53473169ae8fb4f162d3b018771d17b2
lupengfei3 <lupengfei3@guazi.com>
2022-08-19 15:35:10 +0800

    元素icon

--- src/main/java/com/zuoshiyue/genshin/genshin_tool/enums/ElementEnum.java
+++ src/main/java/com/zuoshiyue/genshin/genshin_tool/enums/ElementEnum.java
@@ -15,17 +15,19 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @Getter
 public enum ElementEnum {
-    CRYO("Cryo", "冰"),
-    GEO("Geo", "岩"),
-    ELECTRO("Electro", "雷"),
-    HYDRO("Hydro", "水"),
-    PYRO("Pyro", "火"),
-    ANEMO("Anemo", "风"),
+    CRYO("Cryo", "冰", "https://s1.ax1x.com/2022/08/19/vrHPWq.png"),
+    GEO("Geo", "岩", "https://s1.ax1x.com/2022/08/19/vrHew4.png"),
+    ELECTRO("Electro", "雷", "https://s1.ax1x.com/2022/08/19/vrHEOU.png"),
+    HYDRO("Hydro", "水", "https://s1.ax1x.com/2022/08/19/vrHuk9.png"),
+    PYRO("Pyro", "火", "https://s1.ax1x.com/2022/08/19/vrHMf1.png"),
+    ANEMO("Anemo", "风", "https://s1.ax1x.com/2022/08/19/vrHFS0.png"),
+    DENDRO("dendro", "草", "https://s1.ax1x.com/2022/08/19/vrHklV.png"),
 
     ;
 
     private String name;
     private String desc;
+    private String icon;
 
     public static final Map<String, ElementEnum> ENUM_MAP = new LinkedHashMap<>();
 
@@ -33,7 +35,7 @@ public enum ElementEnum {
         ENUM_MAP.putAll(Arrays.stream(ElementEnum.values()).collect(Collectors.toMap(ElementEnum::getName, item -> item)));
     }
 
-    public static final ElementEnum getElementEnumByName(String name){
+    public static final ElementEnum getElementEnumByName(String name) {
         return ENUM_MAP.get(name);
     }
 
